5f9883cf2959667d2745f90f787efb55e5214be4
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-11 18:08:32 -0500

    start A

--- taller1/A.c
+++ taller1/A.c
@@ -1,4 +1,20 @@
 // https://vjudge.net/contest/713089#problem/A
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
+uint16_t game(char* numerosStr[] ){
+    
+}
+
+int main(){
+    uint8_t n;
+
+    scanf("%hhu", &n);  
+
+    for(uint8_t i = 0; i < n; i++){
+        
+        printf("hola");
+    }
+    return 0;
+}
