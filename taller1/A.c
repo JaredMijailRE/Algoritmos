7be681755bc229e06396ec20ba010357bca080c5
Jared Mijail <125757856+JaredMijailRE@users.noreply.github.com>
2025-05-12 09:15:44 -0500

    Update A.c not run answer written in cellphone

--- taller1/A.c
+++ taller1/A.c
@@ -3,8 +3,28 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-uint16_t game(char* numerosStr[] ){
-    
+
+void minPush(uint8_t arr[], uint8_t j, uint8_t newElem) {
+
+    while (j > 0 && arr[j - 1] > newElem) {
+        arr[j] = arr[j - 1];
+        j--;
+    }
+
+    arr[j] = newElem;
+}
+
+void game(){
+
+uint8_t n;
+scanf("%hhu", &n);
+uint16_t size = 2 * n;
+uint8_t arr[size];
+
+for(uint16_t i = 0; i < size)
+    uint8_t e;
+    scanf("%hhu", e);
+    minPush(arr, i, e)
 }
 
 int main(){
@@ -13,8 +33,7 @@ int main(){
     scanf("%hhu", &n);  
 
     for(uint8_t i = 0; i < n; i++){
-        
-        printf("hola");
+        game();
     }
     return 0;
 }
