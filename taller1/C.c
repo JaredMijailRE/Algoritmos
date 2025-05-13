fefd71cc53775c9459a811740204f13d819d13b6
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-13 01:22:36 -0500

    codigo con error de un caso divicion entera

--- taller1/C.c
+++ taller1/C.c
@@ -1,1 +1,20 @@
 // https://vjudge.net/contest/713089#problem/C
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int main(){
+    uint8_t n;
+    uint8_t k;
+    scanf("%hhu %hhu", &n, &k);
+    uint8_t maxMinutes = 4 * 60;
+    maxMinutes =- k;
+    uint8_t i = maxMinutes / 5;
+    if(i > n){
+        i = n;
+    }
+    printf("%hhu", i);
+
+    return 0;
+}
