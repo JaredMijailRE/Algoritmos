fa08dd1fc9230e3b9cb625722ebec8c209e80340
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-13 01:37:33 -0500

    respuesta correcta c

--- taller1/C.c
+++ taller1/C.c
@@ -13,7 +13,7 @@ int main(){
     uint8_t numExcercise = 0;
 
 
-    uint8_t total = 0;
+    uint16_t total = 0;
     for(uint8_t i = 1; i <= n; i++){
         total += 5 * i;
         if(total <= maxMinutes){
