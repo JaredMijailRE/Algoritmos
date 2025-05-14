c0fb609aa53cc206386c709daf75dedcfa01b25f
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-13 21:22:31 -0500

    el tiempo limite fue excedido D

--- taller1/D.c
+++ taller1/D.c
@@ -18,41 +18,37 @@ struct notiList
     struct D* notis;
 };
 
-void event1(int appNum, struct notiList* listb){   
-    struct notiList list = *listb;
-    struct D d;
-    d.read = false;
-    d.app = appNum;
-    list.notis[list.last] = d;
+void event1(int appNum, struct notiList* list) {
+    struct D d = { .app = appNum, .read = false };
+    list->notis[list->last++] = d;  
 }
 
-void event2(int appNum, struct notiList* listb)
+
+void event2(int appNum, struct notiList* list)
 {
-    struct notiList list = *listb;
-    for( int i = 0; i < list.len; i++){
-        if(list.notis[i].app == appNum){
-            list.notis[i].read = true;
+    for( int i = 0; i < list->len; i++){
+        if(list->notis[i].app == appNum){
+            list->notis[i].read = true;
         }
     }
 };
 
-void event3(int readNoti, struct notiList* listb)
+void event3(int readNoti, struct notiList* list)
 {
-    struct notiList list = *listb;
     for( int i = 0; i < readNoti; i++){
-        list.notis[i].read = true;
+        list->notis[i].read = true;
 
     }
 };
 
 void countNoti(struct notiList list){
     int count = 0;
-    for( int i = 0; i < list.len; i++){
+    for( int i = 0; i < list.last; i++){
         if(!list.notis[i].read){
             count++;
         }
     }
-    printf("%i", count);
+    printf("%i\n", count);
 }
 
 
@@ -65,7 +61,7 @@ int main(){
     struct notiList list;
     struct D noti[k];
     list.last = 0;
-    list.notis = &noti;
+    list.notis = noti;
     list.len = k;
 
     for(uint8_t i = 0; i < k; i++){
@@ -86,6 +82,7 @@ int main(){
             break;
         case 3:
             event3(appNumber, &list);
+            break;
         }
 
         countNoti(list);
