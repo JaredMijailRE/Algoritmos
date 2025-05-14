19f3ec973a1e47881cadcb164e421a632cf6c08b
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-13 22:01:14 -0500

    erro test 4 problem c

--- taller1/D.c
+++ taller1/D.c
@@ -8,53 +8,18 @@ struct D
 {
     int app;
     bool read;
-
 };
 
 struct notiList
-{   
+{
     int last;
     int len;
     int topTrue;
-    struct D* notis;
+    struct D *notis;
 };
 
-void event1(int appNum, struct notiList* list) {
-    struct D d = { .app = appNum, .read = false };
-    list->notis[list->last++] = d;  
-}
-
-
-void event2(int appNum, struct notiList* list)
+int main()
 {
-    for( int i = list->topTrue; i < list->len; i++){
-        if(list->notis[i].app == appNum){
-            list->notis[i].read = true;
-        }
-    }
-};
-
-void event3(int readNoti, struct notiList* list)
-{
-    for( int i = list->topTrue; i < readNoti; i++){
-        list->notis[i].read = true;
-    }
-    list->topTrue = readNoti;
-
-};
-
-void countNoti(struct notiList list){
-    int count = 0;
-    for( int i = list.topTrue; i < list.last; i++){
-        if(!list.notis[i].read){
-            count++;
-        }
-    }
-    printf("%i\n", count);
-}
-
-
-int main(){
     int n; // applications
     int k; // events
     scanf("%d %d", &n, &k);
@@ -66,26 +31,45 @@ int main(){
     list.notis = noti;
     list.len = k;
 
-    for(uint8_t i = 0; i < k; i++){
+    for (uint8_t i = 0; i < k; i++)
+    {
         uint8_t eventType;
         int appNumber;
-        scanf("%hhu %i", &eventType, &appNumber);
+        scanf("%hhu %d", &eventType, &appNumber);
 
         switch (eventType)
         {
         case 1:
-            event1(appNumber, &list);
-            break;
-        case 2:
-            event2(appNumber, &list);
-            break;
-        case 3:
-            event3(appNumber, &list);
+        {
+            struct D d = {.app = appNumber, .read = false};
+            list.notis[list.last++] = d;
             break;
         }
 
-        countNoti(list);
+        case 2:
+            for (int i = list.topTrue; i < list.len; i++)
+            {
+                if (list.notis[i].app == appNumber)
+                {
+                    list.notis[i].read = true;
+                }
+            }
+            break;
 
+        case 3:
+            list.topTrue = appNumber;
+            break;
+        }
+
+        int count = 0;
+        for (int i = list.topTrue; i < list.last; i++)
+        {
+            if (!list.notis[i].read)
+            {
+                count++;
+            }
+        }
+        printf("%i\n", count);
     }
     return 0;
 }
