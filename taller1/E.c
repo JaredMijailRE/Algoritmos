274a8bd3bc2d5550ca278aa9355bc77c4ba334ac
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-14 00:50:05 -0500

    solucion sin problema de planteamientos E

--- taller1/E.c
+++ taller1/E.c
@@ -3,82 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
-{
-    int clave;
-    int valor;
-    struct Node* left;
-    struct Node* right;
-} Node;
-
-Node* createNode(int clave, int valor){
-    Node* node = (Node*)malloc(sizeof(Node));
-    node->clave = clave;
-    node->valor = valor;
-    node->left =  NULL;
-    node->right = NULL;
-
-    return node;
-}
-
-Node* insert(Node* root, int clave, int valor ){
-    if (root == NULL) {
-        return createNode(clave, valor);
+int lower_bound_search(int A[], int N, int x) {
+    int lo = 0, hi = N - 1, pos = -1;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (A[mid] < x) {
+            lo = mid + 1;
+        } else {
+            if (A[mid] == x) pos = mid;
+            hi = mid - 1;
+        }
     }
-
-    if (clave < root->clave) {
-        root->left = insert(root->left, clave, valor);
-    } else if (clave > root->clave) {
-        root->right = insert(root->right, clave, valor);
-    } else {
-        root->valor = valor;
-    }
-
-    return root;
+    return pos;
 }
 
-int search(Node* root, int clave){
-    if( root == NULL)
-        return -1;
-
-    if(root->clave == clave)
-        return root->valor;
-
-    if(clave < root->clave)
-        return search(root->left, clave);
-
-    else
-        return search(root->right, clave);
-}
-void freeMemory(Node* root){
-    if(root){
-        freeMemory(root->left);
-        freeMemory(root->right);
-        free(root);
-    }
-}
-
-int main(){
+int main() {
     int N, Q;
-    scanf("%d %d", &N, &Q);
-
-    Node* root = NULL;
+    if (scanf("%d %d", &N, &Q) != 2) return 0;
     
-    for(int i = 0; i < N; i++){
-        int value;
-        scanf("%d", &value);
-        root = insert(root, value, i);
+    int *A = malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
     }
 
-    // querys
-    for(int i = 0; i < Q; i++){
-        int query;
-        scanf("%d", &query);
-        query = search(root, query);
-        printf("%d\n", query);
+    for (int qi = 0; qi < Q; qi++) {
+        int x;
+        scanf("%d", &x);
+        int ans = lower_bound_search(A, N, x);
+        printf("%d\n", ans);
     }
 
-    freeMemory(root);
-
+    free(A);
     return 0;
 }
