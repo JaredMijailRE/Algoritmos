cefdb71c4a5e8cb896a189f1cfcd0fbc3593f035
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-18 23:42:43 -0500

    cambio problema evaluacion con log, para menor complejidad, fallo time limit

--- taller1/G.c
+++ taller1/G.c
@@ -1,48 +1,34 @@
 // https://vjudge.net/contest/713089#problem/G 
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-unsigned int factorial(int n) {
-    if (n < 0) {
-        return 0;
+void evaluate() {
+    unsigned int a;
+    scanf("%u", &a);
+
+
+    unsigned int n = a * 2;
+    long double loga = logl(a);
+
+    long double sum_log = 0.0L;
+    for (unsigned int i = 1; i <= n; ++i) {
+        sum_log += logl(i);
     }
-    unsigned int resultado = 1;
-    for (int i = 1; i <= n; ++i) {
-        resultado *= i;
+
+    while (sum_log < (long double)n * loga) {
+        ++n;
+        sum_log += logl(n);
     }
-    return resultado;
+
+    printf("%u\n", n);
 }
 
-unsigned int potencia(unsigned int base, unsigned int exponente) {
-    unsigned int resultado = 1;
-
-    for (unsigned int i = 0; i < exponente; ++i) {
-        resultado *= base;
-    }
-
-    return resultado;
-}
-
-void evaluate(){
-    int a;
-    scanf("%d", &a);
-    int n = a*2;
-    while (factorial(n) < potencia(a, n))
-    {
-        n++;
-    }
-    printf("%d\n", n);
-}
-
-int main(){
-    
+int main() {
     int t;
-    scanf("%d", &t);
-
-    for(int i = 0; i < t; i++){
+    if (scanf("%d", &t) != 1) return 0;
+    while (t--) {
         evaluate();
     }
-
     return 0;
 }
