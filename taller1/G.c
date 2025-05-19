cc22a21a39303c5d5cdbec280ba7a4fecef54fd0
JaredMijailRE <jramirezes@unal.edu.co>
2025-05-18 23:38:07 -0500

    primer intento time limit

--- taller1/G.c
+++ taller1/G.c
@@ -1,1 +1,48 @@
-// https://vjudge.net/contest/713089#problem/G
+// https://vjudge.net/contest/713089#problem/G 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int factorial(int n) {
+    if (n < 0) {
+        return 0;
+    }
+    unsigned int resultado = 1;
+    for (int i = 1; i <= n; ++i) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+unsigned int potencia(unsigned int base, unsigned int exponente) {
+    unsigned int resultado = 1;
+
+    for (unsigned int i = 0; i < exponente; ++i) {
+        resultado *= base;
+    }
+
+    return resultado;
+}
+
+void evaluate(){
+    int a;
+    scanf("%d", &a);
+    int n = a*2;
+    while (factorial(n) < potencia(a, n))
+    {
+        n++;
+    }
+    printf("%d\n", n);
+}
+
+int main(){
+    
+    int t;
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++){
+        evaluate();
+    }
+
+    return 0;
+}
