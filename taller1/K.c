23a5a1de16177652930855513135aa8e4d86ba3e
JaredMijailRE <jramirezes@unal.edu.co>
2025-06-02 13:48:36 -0500

    improve int time

--- taller1/K.c
+++ taller1/K.c
@@ -9,19 +9,20 @@ int custom_min(int a, int b) {
 }
 
 void leer_nombre_completo(char *line_input, char *n1, char *n2_first_char, char *s1, char *s2) {
-    char temp_w1[MAX_LEN], temp_w2[MAX_LEN], temp_w3[MAX_LEN], temp_w4[MAX_LEN];
-    int num_words = sscanf(line_input, "%s %s %s %s", temp_w1, temp_w2, temp_w3, temp_w4);
+    char temp1[MAX_LEN], temp2[MAX_LEN], temp3[MAX_LEN], temp4[MAX_LEN];
+    int num_words = sscanf(line_input, "%s %s %s %s", temp1, temp2, temp3, temp4);
+
     if (num_words == 3) {
-        strcpy(n1, temp_w1);
+        strcpy(n1, temp1);
         n2_first_char[0] = '\0';
-        strcpy(s1, temp_w2);
-        strcpy(s2, temp_w3);
+        strcpy(s1, temp2);
+        strcpy(s2, temp3);
     } else if (num_words == 4) {
-        strcpy(n1, temp_w1);
-        n2_first_char[0] = temp_w2[0];
+        strcpy(n1, temp1);
+        n2_first_char[0] = temp2[0];
         n2_first_char[1] = '\0';
-        strcpy(s1, temp_w3);
-        strcpy(s2, temp_w4);
+        strcpy(s1, temp3);
+        strcpy(s2, temp4);
     } else {
         exit(1);
     }
@@ -32,8 +33,9 @@ int consultar_ldap(const char *ldap_buf) {
     printf("%s\n", ldap_buf);
     fflush(stdout);
     if (scanf("%s", respuesta) != 1) exit(1);
-    return respuesta[0] == '1';
+    return respuesta[0] == '1'; 
 }
+
 
 void imprimir_ldap_final(const char *ldap_buf) {
     printf("! %s\n", ldap_buf);
@@ -42,43 +44,53 @@ void imprimir_ldap_final(const char *ldap_buf) {
 
 void generar_y_verificar_ldap(const char *n1, const char *n2_first_char, const char *s1, const char *s2) {
     char n1_prefix[MAX_LEN], s2_prefix[MAX_LEN], ldap_buf[100];
-    int len_n1 = strlen(n1);
-    int len_s2 = strlen(s2);
+    int len_n1 = strlen(n1), len_s2 = strlen(s2);
+    int consultas = 0;
+
     for (int k = 1; k <= len_n1; ++k) {
         strncpy(n1_prefix, n1, k);
         n1_prefix[k] = '\0';
-        int max_m = custom_min(k, len_s2);
+        int max_m = k;
         int start_m = (k == 1 ? 1 : 0);
-        for (int m = start_m; m <= max_m; ++m) {
+
+        for (int m = start_m; m <= custom_min(max_m, len_s2); ++m) {
             if (m == 0) {
                 s2_prefix[0] = '\0';
             } else {
                 strncpy(s2_prefix, s2, m);
                 s2_prefix[m] = '\0';
             }
+
             sprintf(ldap_buf, "%s%s%s%s", n1_prefix, n2_first_char, s1, s2_prefix);
             if (!consultar_ldap(ldap_buf)) {
                 imprimir_ldap_final(ldap_buf);
-                exit(0);
+                return;
             }
+            if (++consultas >= 20) return;
         }
     }
-    for (int m = custom_min(len_n1, len_s2) + 1; m <= len_s2; ++m) {
+
+    
+    for (int m = strlen(n1) + 1; m <= len_s2; ++m) {
         strncpy(s2_prefix, s2, m);
         s2_prefix[m] = '\0';
         sprintf(ldap_buf, "%s%s%s%s", n1, n2_first_char, s1, s2_prefix);
         if (!consultar_ldap(ldap_buf)) {
             imprimir_ldap_final(ldap_buf);
-            exit(0);
+            return;
         }
+        if (++consultas >= 20) return;
     }
 }
 
 int main() {
     char linea[150], n1[MAX_LEN], n2_first_char[2], s1[MAX_LEN], s2[MAX_LEN];
+
     if (fgets(linea, sizeof(linea), stdin) == NULL) return 1;
     linea[strcspn(linea, "\n")] = 0;
+
     leer_nombre_completo(linea, n1, n2_first_char, s1, s2);
     generar_y_verificar_ldap(n1, n2_first_char, s1, s2);
+
     return 0;
 }
