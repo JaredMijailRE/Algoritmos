9076754ea4c309350caba0f49fc04b92813703be
JaredMijailRE <jramirezes@unal.edu.co>
2025-06-02 19:07:03 -0500

    correcion F final

--- taller1/F.py
+++ taller1/F.py
@@ -3,10 +3,10 @@ import sys
 def solve():
     full_name_parts = sys.stdin.readline().strip().split()
     first_name1 = ""
-    first_name2 = "" 
+    first_name2 = ""
     last_name1 = ""
     last_name2 = ""
-    
+
     if len(full_name_parts) == 3:
         first_name1 = full_name_parts[0]
         last_name1 = full_name_parts[1]
@@ -16,42 +16,70 @@ def solve():
         first_name2 = full_name_parts[1]
         last_name1 = full_name_parts[2]
         last_name2 = full_name_parts[3]
-    
+
     f2_char_component = ""
     if first_name2:
         f2_char_component = first_name2[0]
-    
+
+    L = len(first_name1)
+    M = len(last_name2)
+
+    groups = []
+    groups.append((1, 1, 1))  
+
+    for a in range(2, L + 1):
+        count = min(a + 1, M + 1)
+        groups.append((a, 0, count))
+
+    if M > L:
+        groups.append((L, L + 1, M - L))
+
+    total_indices = sum(g[2] for g in groups)
+
+    def build_ldap(a, b):
+        part1 = first_name1[:a]
+        part2 = f2_char_component
+        part3 = last_name1
+        part4 = last_name2[:b] if b > 0 else ""
+        return part1 + part2 + part3 + part4
+
+    def get_ldap_by_index(index):
+        count = 0
+        for a, b_start, length in groups:
+            if index < count + length:
+                b_val = b_start + (index - count)
+                return build_ldap(a, b_val)
+            count += length
+        return None
+
     def make_query(ldap_str):
-        print(ldap_str, flush=True)
-        response = sys.stdin.readline().strip()
-        if response == "0":
-            print(f"! {ldap_str}", flush=True)
-            sys.exit(0)
-    
-    if not first_name1 or not last_name1 or not last_name2:
-        sys.exit("Error: Partes del nombre faltantes para el LDAP inicial.")
-        
-    ldap_seq1 = first_name1[0] + f2_char_component + last_name1 + last_name2[0]
-    make_query(ldap_seq1)
-    
-    for l_f1_len in range(2, len(first_name1) + 1):
-        current_fn1_prefix = first_name1[:l_f1_len]
-        
-        limit_l_l2_len = min(l_f1_len, len(last_name2))
-        for l_l2_len in range(limit_l_l2_len + 1):
-            current_ln2_prefix = last_name2[:l_l2_len]
-            ldap = current_fn1_prefix + f2_char_component + last_name1 + current_ln2_prefix
-            make_query(ldap)
-    
-    if len(first_name1) > 0:
-        fn1_full_prefix = first_name1
-        
-        start_l_l2_len_final = min(len(first_name1), len(last_name2)) + 1
-        
-        for l_l2_len in range(start_l_l2_len_final, len(last_name2) + 1):
-            current_ln2_prefix = last_name2[:l_l2_len]
-            ldap = fn1_full_prefix + f2_char_component + last_name1 + current_ln2_prefix
-            make_query(ldap)
+        print(ldap_str)
+        sys.stdout.flush()
+        while True:
+            response = sys.stdin.readline().strip()
+            if response:
+                return response
+
+    left = 0
+    right = total_indices - 1
+    result_index = None
+
+    while left <= right:
+        mid = (left + right) // 2
+        candidate = get_ldap_by_index(mid)
+        response = make_query(candidate)
+        if response == "1":
+            left = mid + 1
+        else:
+            result_index = mid
+            right = mid - 1
+
+    if result_index is None:
+        result_index = total_indices - 1
+
+    final_ldap = get_ldap_by_index(result_index)
+    print(f"! {final_ldap}")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     solve()
