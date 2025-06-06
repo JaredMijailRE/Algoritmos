09b6ba937f1df373325e9e9da2b105b82e5fcab5
JaredMijailRE <jramirezes@unal.edu.co>
2025-06-06 15:41:37 -0500

    se paso todo a int para evitar problemas taller 2 B

--- taller2/B.cpp
+++ taller2/B.cpp
@@ -4,40 +4,33 @@
 
 #include <iostream>
 #include <vector>
-#include <stdio.h>
-#include <stdint.h>
 #include <string>
 #include <tuple>
 #include <queue>
-#include <map>
 #include <algorithm>
 
 using namespace std;
-
-// definimos el tipo matrix
 using Matrix = vector<vector<char>>;
-using u16 = uint_fast16_t;
-using point = tuple<u16, u16>;
-
+using point = tuple<int, int>;
 
 Matrix getBuilding() {
-    u16 height, width;
+    int height, width;
     cin >> height >> width;
 
     Matrix building(height, vector<char>(width));
     string row;
-    for (u16 i = 0; i < height; i++) {
-        cin >> row;  // leemos exactamente m caracteres
-        for (u16 j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++) {
+        cin >> row;  
+        for (int j = 0; j < width; j++) {
             building[i][j] = row[j];
         }
     }
     return building;
 }
 
-void occupyRoom(Matrix &building, u16 x, u16 y){
-    u16 height = building.size();
-    u16 width = building[0].size();
+void occupyRoom(Matrix &building, int x, int y){
+    int height = building.size();
+    int width = building[0].size();
 
     const int dx[] = {-1, 1, 0, 0};
     const int dy[] = {0, 0, -1, 1};
@@ -51,40 +44,35 @@ void occupyRoom(Matrix &building, u16 x, u16 y){
         q.pop();
 
         for (int dir = 0; dir < 4; dir++) {
-            int nx = int(cx) + dx[dir];
-            int ny = int(cy) + dy[dir];
+            int nx = cx + dx[dir];
+            int ny = cy + dy[dir];
 
-            if (nx >= 0 && nx < static_cast<int>(height) && ny >= 0 && ny < static_cast<int>(width) && building[nx][ny] == '.') {
+            if (nx >= 0 && nx < height && ny >= 0 && ny < width && building[nx][ny] == '.') {
                 building[nx][ny] = '#';
-                q.push({u16(nx), u16(ny)});
+                q.push({nx, ny});
             }
         }
     }
 }
 
 void countRooms(Matrix &building){
-    u16 height = building.size();
-    u16 width = building[0].size();
+    int height = building.size();
+    int width = building[0].size();
 
-    uint_fast8_t roomCounter = 0;
-    for (u16 i = 0; i < height; i++) {
-        for (u16 j = 0; j < width; j++) {
+    int roomCounter = 0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             if (building[i][j] == '.'){
                 roomCounter += 1;
                 occupyRoom(building, i, j);
             }
         }
     }
-    cout << static_cast<int>(roomCounter) << endl;
+    cout << roomCounter << endl;
 }
 
-
-
-
 int main() {
-    
     Matrix building = getBuilding();
     countRooms(building);
-
     return 0;
 }
