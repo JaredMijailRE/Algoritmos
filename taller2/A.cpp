a0a5541991e22786af6dbfebdfe5b057212c8eb0
turing <jramirezes@unal.edu.co>
2025-06-05 20:19:40 -0500

    entrada del problema

--- taller2/A.cpp
+++ taller2/A.cpp
@@ -4,10 +4,38 @@
 
 
 #include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdint.h>
+#include <cstdio>
+#include <string>
 using namespace std;
 
+// definimos el tipo matrix
+using Matrix = vector<vector<char>>;
+using u16 = uint_fast16_t;
+
+
+Matrix getLabyrinth(){
+    u16 height, width;
+    cin >> height >> width;
+    cin.ignore();
+    
+    Matrix labyrinth;
+    getchar(); // consumimos el salto de linea
+    string row;
+    for (u16 i = 0; i < height; i++) {
+        getline(cin, row);
+        labyrinth.push_back(vector<char>(row.begin(), row.end()));
+    }
+
+    return labyrinth;
+}
 
 int main() {
 
+    Matrix laberinto = getLabyrinth();
+
+    
     return 0;
 }
